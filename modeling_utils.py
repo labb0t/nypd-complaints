4fec3f5af776d6f4eeca0f6ae3789ccacf138b9e
Lucy Abbot <lucy.abbot@gmail.com>
2020-10-26 23:26:12 -0700

    additional modeling, eda

--- modeling_utils.py
+++ modeling_utils.py
@@ -54,14 +54,15 @@ def get_classifier_performance(X,y,model,preprocessor):
     y_cv_pred_proba = cross_val_predict(clf, X, y, cv=5,method='predict_proba')
 
     # generate classification report
-    print(classification_report(y, y_cv_pred))
+    print(classification_report(y, y_cv_pred))    
 
+    # generate and plot confusion matrix
     fig, axes = plt.subplots(nrows=1, ncols=1)
 
-    # generate confusion matrix
     cf_matrix = confusion_matrix(y, y_cv_pred)
     sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
                 fmt='.0%', cmap='Purples')
+    plt.title('Correlation Matrix')
     
     # plot roc curve
     clf_roc_auc = roc_auc_score(y, y_cv_pred)
@@ -76,3 +77,64 @@ def get_classifier_performance(X,y,model,preprocessor):
     plt.title('ROC Curve - Ruling: Conduct Occurred')
     plt.legend(loc="lower right")
     plt.show()
+
+def get_feature_names(column_transformer):
+    """Get feature names from all transformers.
+    Returns
+    -------
+    feature_names : list of strings
+        Names of the features produced by transform.
+    """
+    # Remove the internal helper function
+    #check_is_fitted(column_transformer)
+    
+    # Turn loopkup into function for better handling with pipeline later
+    def get_names(trans):
+        # >> Original get_feature_names() method
+        if trans == 'drop' or (
+                hasattr(column, '__len__') and not len(column)):
+            return []
+        if trans == 'passthrough':
+            if hasattr(column_transformer, '_df_columns'):
+                if ((not isinstance(column, slice))
+                        and all(isinstance(col, str) for col in column)):
+                    return column
+                else:
+                    return column_transformer._df_columns[column]
+            else:
+                indices = np.arange(column_transformer._n_features)
+                return ['x%d' % i for i in indices[column]]
+        if not hasattr(trans, 'get_feature_names'):
+        # >>> Change: Return input column names if no method avaiable
+            # For transformers without a get_features_names method, use the input
+            # names to the column transformer
+            if column is None:
+                return []
+            else:
+                return [name + "__" + f for f in column]
+
+        return [name + "__" + f for f in trans.get_feature_names()]
+    
+    ### Start of processing
+    feature_names = []
+    
+    # Allow transformers to be pipelines. Pipeline steps are named differently, so preprocessing is needed
+    if type(column_transformer) == Pipeline:
+        l_transformers = [(name, trans, None, None) for step, name, trans in column_transformer._iter()]
+    else:
+        # For column transformers, follow the original method
+        l_transformers = list(column_transformer._iter(fitted=True))
+    
+    
+    for name, trans, column, _ in l_transformers: 
+        if type(trans) == Pipeline:
+            # Recursive call on pipeline
+            _names = get_feature_names(trans)
+            # if pipeline has no transformer that returns names
+            if len(_names)==0:
+                _names = [name + "__" + f for f in column]
+            feature_names.extend(_names)
+        else:
+            feature_names.extend(get_names(trans))
+    
+    return feature_names
